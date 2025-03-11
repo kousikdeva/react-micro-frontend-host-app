070148e81a80e10ae279a30f60fdb24d54018c5e
Kousik <kousikdeva@gmail.com>
2025-03-11 09:48:48 +0530

    added error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { Suspense } from "react";
+import ErrorBoundary from "./components/ErrorBounary";
 
 const Button = React.lazy(() => import("remoteApp/Button"))
 
 const App: React.FC = () => {
     return <>
         <h1>Hello, React MicroFE Host App</h1>
-        <Suspense fallback={<div>Loading Button...</div>}>
-            <Button onClick={() => console.log('clicked')}>Click me</Button>
-        </Suspense>
+        <ErrorBoundary fallback={<h1>Error...</h1>}>
+            <Suspense fallback={<div>Loading Button...</div>}>
+                <Button onClick={() => console.log('clicked')}>Click me</Button>
+            </Suspense>
+        </ErrorBoundary>
     </>;
 };
 
---
+++ src/components/ErrorBounary.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+interface ErrorBoundaryProps {
+    fallback: React.ReactNode;
+    children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+        console.error("ErrorBoundary caught an error", error, errorInfo);
+    }
+    render() {
+        if (this.state.hasError) {
+            return this.props.fallback;
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
